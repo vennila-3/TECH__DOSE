f5602b8b2fbf88a044cd8358934c21cb36fffe80
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-09-10 22:12:55 +0530

    Create PatternPrintReverse.java

---
+++ BASICS/PatternPrintReverse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++)
+		{
+		    
+		    for(int j=5;j>=i;j--)
+		
+		    {
+		        System.out.print(j);
+		       
+		    }
+		    System.out.print("\n");
+		}
+		
+	}
+}
