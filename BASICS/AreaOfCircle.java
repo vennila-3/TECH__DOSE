4299a263ff114005d67bbd0fcbece28882ac0387
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-09-10 22:08:51 +0530

    Create AreaOfCircle.java

---
+++ BASICS/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{  
+	public static void main(String[] args) { 
+	    Scanner sc=new Scanner(System.in);
+	    int r=sc.nextInt(); 
+	    float area=(float)(3.14*r*r);
+		System.out.println(area);
+	}
+}
