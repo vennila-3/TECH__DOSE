ea1f4b4b372bc38380b1c2ddceb71910d2eb8e59
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-09-10 22:21:49 +0530

    Create check perfect square or not.java

---
+++ BASICS/check perfect square or not.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;  
+public class Main{  
+static boolean checkPerfectSquare(double number)    
+{   
+double sqrt=Math.sqrt(number);     
+return ((sqrt - Math.floor(sqrt)) == 0);   
+}   
+public static void main(String[] args)    
+{   
+System.out.print("Enter any number: ");  
+Scanner sc=new Scanner(System.in);  
+double number=sc.nextDouble();   
+if (checkPerfectSquare(number))   
+System.out.print("Yes, the given number is perfect square.");   
+else  
+System.out.print("No, the given number is not perfect square.");   
+}   
+}
