be5b7bd69ecd90b8cf04dbaedc981c878347fd41
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-09-10 22:06:09 +0530

    Create GCD.java

---
+++ BASICS/GCD.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{   static int findGCD(int a,int b)
+{
+    if(b==0)
+    return a;
+    else
+    return findGCD(b,a%b);
+}
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int gcd=findGCD(a,b);
+		System.out.println("GCD = "+gcd);
+		
+	}
+}
