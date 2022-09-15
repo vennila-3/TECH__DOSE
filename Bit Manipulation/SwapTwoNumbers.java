8415f9ad695a0898b329107d04fa436eba56881f
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-09-15 10:00:22 +0530

    Update and rename Single Number to SwapTwoNumbers.java

---
+++ Bit Manipulation/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+public class SwapTwoNumbers {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a+" "+b);
+    }
+    
+}
