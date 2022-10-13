130e356c5618941edc4edf26e0669143c11eaec0
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-10-13 11:27:38 +0530

    Create 2404. Most Frequent Even Element.java

---
+++ leetcode/2404. Most Frequent Even Element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+            
+        HashMap<Integer,Integer>counts =new HashMap<>();
+            for(int i:nums)
+            {
+            if(i%2==0)                    
+                    counts.put(i,counts.getOrDefault(i,0)+1);                                               }
+            int ans=-1 ,maxFreq=-1;
+            
+            for(Integer num:counts.keySet()){
+                    
+                if(counts.get(num)>maxFreq){
+                   maxFreq=counts.get(num);
+                        ans=num;
+                }
+                else if(counts.get(num)==maxFreq && ans>num){
+                        ans=num;                        
+                }                    
+            }
+            return ans;
+    }
+}
