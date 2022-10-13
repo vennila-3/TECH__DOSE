905e7d71a9dab38e0621754848ee995c9d2936f8
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-10-13 11:56:08 +0530

    Create biweekly 303.java

---
+++ leetcode/biweekly 303.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+        public char repeatedCharacter(String s) {
+        Set<Character> set = new HashSet();
+        for (char c : s.toCharArray())  
+            if (!set.add(c)) 
+                return c;
+				
+        return 'a';// can't reach to this line, because there must be a letter appearing TWICE
+    }
+        
+    
+}
