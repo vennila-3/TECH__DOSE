2d4a10c707d85cab09750f64e52e74ae47c471bd
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-10-21 15:09:11 +0530

    Create IntegertoRoman.java

---
+++ LEETCODE(2)/IntegertoRoman.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] roman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD" ,"D", "CM", "M"};
+        for(int i = values.length - 1; i >= 0 && num > 0; i--){
+
+            while(num >= values[i]){
+                num -= values[i];
+                sb.append(roman[i]);
+            }
+        }
+
+       return sb.toString(); 
+    }
+}
