41532e3abce462222c32a1f93f3d0ea7a32042b9
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-10-21 14:36:52 +0530

    Create ImplementationOfStackUsingQueue.java

---
+++ STACK and QUEUE/ImplementationOfStackUsingQueue.java
@@ -0,0 +1,33 @@
+class MyStack {
+   Queue<Integer> q;
+
+    public MyStack() {
+        this.q=new LinkedList<Integer>();
+    }
+    
+    public void push(int x) {
+        q.add(x);//adding elements
+        for(int i=0;i<q.size()-1;i++)
+        {
+            q.add(q.poll());
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+       
+        
+    }
+    
+    public int top() {
+        return q.peek();
+       
+        
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+       
+        
+    }
+}
